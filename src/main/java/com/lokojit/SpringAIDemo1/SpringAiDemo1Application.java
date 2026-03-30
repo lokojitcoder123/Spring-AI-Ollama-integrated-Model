ac332847cd64dfbffc268a7fa292fca1e4e07d8c
Lokojit Biswas <lokojitb@gmail.com>
2026-03-30 18:04:08 +0530

    Add files via upload

---
+++ src/main/java/com/lokojit/SpringAIDemo1/SpringAiDemo1Application.java
@@ -0,0 +1,13 @@
+package com.lokojit.SpringAIDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lokojit/SpringAIDemo1/SpringAiDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokojit.SpringAIDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
